5d4d1071ae046d6b41ad682fd578f8eb49ceef0b
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-05 18:49:44 +0200

    refactor exercise1 to 2 + readme

--- basic-exercises-part-1/src/exercises/exercise2/Exercise2.java
+++ basic-exercises-part-1/src/exercises/exercise2/Exercise2.java
@@ -1,14 +1,15 @@
-package exercises;
+package exercises.exercise2;
 
 import java.util.Scanner;
 
-public class Exercise1 {
+public class Exercise2 {
     private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.print("Input the first number : ");
         int firstNumber = input.nextInt();
         System.out.print("Input the second number : ");
         int secondNumber = input.nextInt();
-        System.out.print("Sum: "+(firstNumber+secondNumber));
+        System.out.print("Sum: " + (firstNumber + secondNumber));
     }
 }
---
+++ basic-exercises-part-1/src/exercises/exercise2/README.md
@@ -0,0 +1,13 @@
+### [Java Basic: Exercise-2](https://www.w3resource.com/java-exercises/basic/java-basic-exercise-2.php)
+
+***
+Write a Java program to print the sum of two numbers.
+***
+_Sample Output:_
+<pre>
+Input the first number:  256
+Input the second number:  326  
+Sum: 582
+</pre>
+
+
