1d6671011f3e0467045f83b54d2db41fc526101e
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-05 19:20:30 +0200

    solve exercise 4

---
+++ basic-exercises-part-1/src/exercises/exercise4/Exercise4.java
@@ -0,0 +1,20 @@
+package exercises.exercise4;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        int result = -5 + 8 * 6;
+        System.out.println(result);
+        result = (55 + 9) % 9;
+        System.out.println(result);
+        result = 20 + -3 * 5 / 8;
+        System.out.println(result);
+        result = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.print(result);
+    }
+    /*
+     * a. -5 + 8 * 6
+     * b. (55+9) % 9
+     * c. 20 + -3*5 / 8
+     * d. 5 + 15 / 3 * 2 - 8 % 3
+     */
+}
---
+++ basic-exercises-part-1/src/exercises/exercise4/README.md
@@ -0,0 +1,19 @@
+### [Java Basic: Exercise-4](https://www.w3resource.com/java-exercises/basic/java-basic-exercise-4.php)
+
+***
+Write a Java program to print the result of the following operations.
+_Test Data:_
+
+* a. -5 + 8 * 6
+* b. (55+9) % 9
+* c. 20 + -3*5 / 8
+* d. 5 + 15 / 3 * 2 - 8 % 3
+
+***
+_Expected Output :_
+<pre>
+43
+1
+19
+13
+</pre>
