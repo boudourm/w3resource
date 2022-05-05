56346632ba0d6aea04d5a8a923f2d56e89a54877
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-06 00:32:50 +0200

    solve exercise 8

---
+++ basic-exercises-part-1/src/exercises/exercise8/Exercise8.java
@@ -0,0 +1,11 @@
+package exercises.exercise8;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        String asciiArt = "    J    a   v     v  a" + "\n";
+        asciiArt += "    J   a a   v   v  a a" + "\n";
+        asciiArt += "J   J  aaaaa   V V  aaaaa" + "\n";
+        asciiArt += "  JJ  a     a   V  a     a";
+        System.out.print(asciiArt);
+    }
+}
---
+++ basic-exercises-part-1/src/exercises/exercise8/README.md
@@ -0,0 +1,11 @@
+### [Java Basic: Exercise-8](https://www.w3resource.com/java-exercises/basic/java-basic-exercise-8.php)
+
+***
+Write a Java program to display the following pattern.
+***
+_Sample Pattern :_
+
+         J    a   v     v  a
+         J   a a   v   v  a a
+     J   J  aaaaa   V V  aaaaa
+       JJ  a     a   V  a     a
