0e52b6464e13003ed3a9cdef59ddec46bfb8a1a5
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-07 18:49:32 +0200

    update generator

--- basic-exercises-part-1/src/utils/ExerciseGenerator.java
+++ basic-exercises-part-1/src/utils/ExerciseGenerator.java
@@ -28,7 +28,10 @@ public class ExerciseGenerator {
                 newExerciseOrder = order + 1;
         }
         //README.md
-        String content = "###[]()\n***\n\n***\n__\n<pre>\n\n</pre>";
+        //### [Java Basic: Exercise-37](
+        String solution = "https://www.w3resource.com/java-exercises/basic/java-basic-exercise-" + newExerciseOrder + ".php";
+        String content = "###[Java Basic: Exercise-" + newExerciseOrder + "](" + solution + ")\n***\n\n***\n" +
+                "_Sample output:_\n<pre>\n\n</pre>";
         String url = "./basic-exercises-part-1/src/exercises/exercise" + newExerciseOrder;
         new File(url).mkdirs();
         url += "/README.md";
@@ -37,7 +40,9 @@ public class ExerciseGenerator {
 
         //Exercisei.java
         content = "package exercises.exercise" + newExerciseOrder + ";\n\n" +
+                "import java.util.Scanner;\n\n" +
                 "public class Exercise" + newExerciseOrder + " {\n" +
+                "private static final Scanner input = new Scanner(System.in);\n\n" +
                 "    public static void main(String[] args) {\n\n" +
                 "    }\n}";
         url = "./basic-exercises-part-1/src/exercises/exercise" + newExerciseOrder + "/Exercise" + newExerciseOrder + ".java";
