d7d06ff05db6cec95cd5dcaa98c0490e4779fb2f
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-08 10:13:35 +0200

    solve exercise 46

---
+++ basic-exercises-part-1/src/exercises/exercise46/Exercise46.java
@@ -0,0 +1,10 @@
+package exercises.exercise46;
+
+import java.util.Date;
+
+public class Exercise46 {
+
+    public static void main(String[] args) {
+        System.out.print("Current Date time: " + new Date());
+    }
+}
