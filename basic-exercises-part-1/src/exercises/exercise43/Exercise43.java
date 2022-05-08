a5a0f90ae3e172cb7af524223b976bc19ecbeb6a
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-08 09:25:49 +0200

    solve exercise 43

---
+++ basic-exercises-part-1/src/exercises/exercise43/Exercise43.java
@@ -0,0 +1,13 @@
+package exercises.exercise43;
+
+public class Exercise43 {
+
+    public static void main(String[] args) {
+        System.out.print("Twinkle, twinkle, little star,\n" +
+                "\tHow I wonder what you are! \n" +
+                "\t\tUp above the world so high,   \t\t\n" +
+                "\t\tLike a diamond in the sky. \n" +
+                "Twinkle, twinkle, little star, \n" +
+                "\tHow I wonder what you are");
+    }
+}
---
+++ basic-exercises-part-1/src/exercises/exercise43/README.md
@@ -0,0 +1,14 @@
+### [Java Basic: Exercise-43](https://www.w3resource.com/java-exercises/basic/java-basic-exercise-43.php)
+
+***
+Write a Java program to print the following string in a specific format (see the output).
+***
+_Sample output:_
+<pre>
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are
+</pre>
