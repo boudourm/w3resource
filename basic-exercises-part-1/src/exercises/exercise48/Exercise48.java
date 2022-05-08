3d40e73c05f9f677903613fdcf8beff7ea6a9127
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-08 10:50:24 +0200

    solve exercise 48

---
+++ basic-exercises-part-1/src/exercises/exercise48/Exercise48.java
@@ -0,0 +1,9 @@
+package exercises.exercise48;
+
+public class Exercise48 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 99; i = i + 2)
+            System.out.println(i);
+    }
+}
