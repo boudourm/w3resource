198ac39d48cf9452efad8c1c0cc6eb67cb575379
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-08 21:43:24 +0200

    solve exercise 51

---
+++ basic-exercises-part-1/src/exercises/exercise51/Exercise51.java
@@ -0,0 +1,12 @@
+package exercises.exercise51;
+
+import java.util.Scanner;
+
+public class Exercise51 {
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Input a number(string): ");
+        System.out.print("Integer value is: " + Integer.parseInt(input.nextLine()));
+    }
+}
---
+++ basic-exercises-part-1/src/exercises/exercise51/README.md
@@ -0,0 +1,10 @@
+### [Java Basic: Exercise-51](https://www.w3resource.com/java-exercises/basic/java-basic-exercise-51.php)
+
+***
+<p>Write a Java program to convert a string to an integer in Java.</p>
+
+***
+_Sample output:_
+<pre class="output">Input a number(string): 25                                             
+The integer value is: 25
+</pre>
