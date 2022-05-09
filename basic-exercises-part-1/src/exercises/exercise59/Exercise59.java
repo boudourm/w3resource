6b6334d4d7e1e287f8e1d3196d3f093ab8ef0619
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-09 15:58:38 +0200

    solve exercise 59

---
+++ basic-exercises-part-1/src/exercises/exercise59/Exercise59.java
@@ -0,0 +1,12 @@
+package exercises.exercise59;
+
+import java.util.Scanner;
+
+public class Exercise59 {
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Input a String: ");
+        System.out.print(input.nextLine().toLowerCase());
+    }
+}
