1619891dfa8571685b3ca8c075d7d6aa2da280a3
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-14 09:26:42 +0200

    solve exercise 89

---
+++ basic-exercises-part-1/src/exercises/exercise89/Exercise89.java
@@ -0,0 +1,11 @@
+package exercises.exercise89;
+
+import java.util.Scanner;
+
+public class Exercise89 {
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("System secutiy interface : \n" + System.getSecurityManager());
+    }
+}
