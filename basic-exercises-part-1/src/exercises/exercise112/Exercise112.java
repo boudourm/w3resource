0726019e01a7db89204cb4f1db2a2b78b6e10c8f
Mehdi BOUDOUR <60145103+boudourm@users.noreply.github.com>
2022-05-17 21:41:44 +0200

    solve exercise 112

---
+++ basic-exercises-part-1/src/exercises/exercise112/Exercise112.java
@@ -0,0 +1,19 @@
+package exercises.exercise112;
+
+import java.util.Scanner;
+
+public class Exercise112 {
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Input a number : ");
+        int number = input.nextInt();
+        long buffer = number;
+        int trailingZeros = 0;
+        while (buffer > 0) {
+            trailingZeros += buffer / 5;
+            buffer /= 5;
+        }
+        System.out.print("Number of trainling zeos of the factorial of " + number + " is : " + trailingZeros);<
+    }
+}
---
+++ basic-exercises-part-1/src/exercises/exercise112/README.md
@@ -0,0 +1,10 @@
+### [Java Basic: Exercise-112](https://www.w3resource.com/java-exercises/basic/java-basic-exercise-112.php)
+
+***
+<p>Write a Java program to compute the number of trailing zeros in a factorial.</p>
+
+***
+_Sample output:_
+<pre class="output">Input a number : 5040                                                  
+Number of trailing zeros of the factorial 5040 is 1258  
+</pre>
